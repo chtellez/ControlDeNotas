72008b074f170e678a39e6eaa79f7a965080648f
Christian Tellez <ch.tellez@hotmail.com>
2025-09-26 15:22:11 -0500

    cree los objetos Estudiante y Materia

---
+++ aplicativo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+class Estudiante {
+
+    public:
+        string Nombre;
+        float Nota;
+
+};
+
+class Materia
+{
+    public:
+        string Nombre;
+        float Nota;
+        string Tematica;
+};
+
+
+
+void main(){
+    Estudiante Estudiante1;
+    Estudiante Estudiante2;
+
+    Estudiante1.Nombre = "Felipe";
+    Estudiante2.Nombre = "Juan";
+
+    cout << Estudiante1.Nombre << endl;
+    cout << Estudiante2.Nombre << endl;
+
+
+};
